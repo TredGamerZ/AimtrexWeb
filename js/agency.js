354e195c6992d00c415922670508b8af32cfafe6
tanmay <tanmaysingh1226@gmail.com>
2018-08-10 13:36:57 +0530

    CBT Images Updated

--- js/agency.js
+++ js/agency.js
@@ -48,3 +48,9 @@
   })
 
 })(jQuery); // End of use strict
+
+$('.dropdown-toggle').click(function() {
+    var location = $(this).attr('href');
+    window.location.href = location;
+    return false;
+});
